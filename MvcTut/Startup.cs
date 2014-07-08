4aa452f67936f1123f289ec11a2b87228433ee75
emaszs <phonixreal@gmail.com>
2014-07-08 20:02:41 +0300

    Added a view

---
+++ MvcTut/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcTut.Startup))]
+namespace MvcTut
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
